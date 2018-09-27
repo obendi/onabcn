afb945de6e4b820b98192750a15856a74853afc1
Oscar <obendicho@opentrends.net>
2018-09-27 12:44:50 +0200

    Load data just in the first call

--- src/main/java/net/bendi/onabcn/service/ForecastService.java
+++ src/main/java/net/bendi/onabcn/service/ForecastService.java
@@ -56,65 +56,68 @@ public class ForecastService {
 	}
 	
 	public void getServerData() {
-		RestTemplate restTemplate = new RestTemplate();
-		String url = "http://calipso.puertos.es/TablaAccesoSimplificado/util/get_wanadata.php";
 		
-		MultiValueMap<String, String> parametersMap = new LinkedMultiValueMap<String, String>();
-		parametersMap.add("point", "2111136");
-		parametersMap.add("name", "Barcelona");
-		
-		Date lastUpdate = new Date();
-		String data = restTemplate.postForObject(url, parametersMap, String.class);
-		
-		Document document = Jsoup.parse(data);
-		Elements tbody = document.getElementsByTag("tbody");
+		if (this.forecastRepository.count() == 0) {
+			RestTemplate restTemplate = new RestTemplate();
+			String url = "http://calipso.puertos.es/TablaAccesoSimplificado/util/get_wanadata.php";
 			
-		Elements reportItems = tbody.get(1).getElementsByTag("tr");
-		for (Element reportItem:reportItems) {
+			MultiValueMap<String, String> parametersMap = new LinkedMultiValueMap<String, String>();
+			parametersMap.add("point", "2111136");
+			parametersMap.add("name", "Barcelona");
 			
-			Forecast forecastItem = new Forecast();
+			Date lastUpdate = new Date();
+			String data = restTemplate.postForObject(url, parametersMap, String.class);
 			
-			Elements reportItemValues = reportItem.getElementsByTag("td");
-			int index = 0;
-			
-			for (Element reportItemValue:reportItemValues) {
-
-				if (index == 0) {
-					//  Fecha: 2018-09-11 13:00:00
-					SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-					Date date;
-					try {
-						date = formatter.parse(reportItemValue.text());
-						forecastItem.setDate(date);
-					} catch (ParseException e) {
-						e.printStackTrace();
+			Document document = Jsoup.parse(data);
+			Elements tbody = document.getElementsByTag("tbody");
+				
+			Elements reportItems = tbody.get(1).getElementsByTag("tr");
+			for (Element reportItem:reportItems) {
+				
+				Forecast forecastItem = new Forecast();
+				
+				Elements reportItemValues = reportItem.getElementsByTag("td");
+				int index = 0;
+				
+				for (Element reportItemValue:reportItemValues) {
+	
+					if (index == 0) {
+						//  Fecha: 2018-09-11 13:00:00
+						SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+						Date date;
+						try {
+							date = formatter.parse(reportItemValue.text());
+							forecastItem.setDate(date);
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
 					}
+					else if (index == 4) {
+						// Mar total: 0.43
+						Float height = new Float(reportItemValue.text());
+						forecastItem.setTotalHeight(height);
+					}
+					else if (index == 8) {
+						// Mar de viento
+						Float windSwell = new Float(reportItemValue.text().equals("") ? "0" : reportItemValue.text());
+						forecastItem.setWindSwellHeight(windSwell);
+					}
+					else if (index == 10) {
+						// Mar primario
+						Float primarySwell = new Float(reportItemValue.text());
+						forecastItem.setPrimarySwellHeight(primarySwell);
+					}
+					else if (index == 13) {
+						// Mar secundario
+						Float secondarySwell = new Float(reportItemValue.text().equals("") ? "0" : reportItemValue.text());
+						forecastItem.setSecondarySwellHeight(secondarySwell);
+					}
+					
+					forecastItem.setLastUpdate(lastUpdate);
+					forecastRepository.save(forecastItem);
+					
+					index++;
 				}
-				else if (index == 4) {
-					// Mar total: 0.43
-					Float height = new Float(reportItemValue.text());
-					forecastItem.setTotalHeight(height);
-				}
-				else if (index == 8) {
-					// Mar de viento
-					Float windSwell = new Float(reportItemValue.text().equals("") ? "0" : reportItemValue.text());
-					forecastItem.setWindSwellHeight(windSwell);
-				}
-				else if (index == 10) {
-					// Mar primario
-					Float primarySwell = new Float(reportItemValue.text());
-					forecastItem.setPrimarySwellHeight(primarySwell);
-				}
-				else if (index == 13) {
-					// Mar secundario
-					Float secondarySwell = new Float(reportItemValue.text().equals("") ? "0" : reportItemValue.text());
-					forecastItem.setSecondarySwellHeight(secondarySwell);
-				}
-				
-				forecastItem.setLastUpdate(lastUpdate);
-				forecastRepository.save(forecastItem);
-				
-				index++;
 			}
 		}
 	}
