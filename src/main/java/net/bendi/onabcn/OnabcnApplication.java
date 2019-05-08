dfd0f079639efbfadca034934722f2cbceab36cb
Oscar <oscar.bendicho@gmail.com>
2019-05-08 23:49:27 +0200

    More

--- src/main/java/net/bendi/onabcn/OnabcnApplication.java
+++ src/main/java/net/bendi/onabcn/OnabcnApplication.java
@@ -1,10 +1,13 @@
 package net.bendi.onabcn;
 
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +15,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class OnabcnApplication {
+	
+	private static final Logger logger = LogManager.getLogger(OnabcnApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnabcnApplication.class, args);
@@ -19,6 +24,9 @@ public class OnabcnApplication {
 	
 	@PostConstruct
 	public void init() {
+		
+		logger.info("System default TimeZone: {}", ZoneId.systemDefault());
 		TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
+		logger.info("Set system default TimeZone to: {}", ZoneId.systemDefault());
 	}
 }
